01bd5d2b3461b01b86179adf97750d5c10b48adb
AthaRzre <athafadhilah01@gmail.com>
2024-09-04 17:37:12 +0700

    Mengubah Script Urutan pemain

--- Assets/Script/Pemain.cs
+++ Assets/Script/Pemain.cs
@@ -7,8 +7,31 @@ public class Pemain : MonoBehaviour
     [SerializeField]
     public string ID_Pemain;
 
+    // Daftar objek pemain lainnya yang harus diaktifkan/nonaktifkan
+    //[SerializeField]
+    //private List<GameObject> pemainList;
+
     public void MainkanGiliran()
     {
         Debug.Log("Pemain " + ID_Pemain + " Mulai Bermain");
+        // Aktifkan atau nonaktifkan objek berdasarkan giliran pemain
+        //UpdatePemainList();
     }
+
+    // Method untuk mengaktifkan game object berdasarkan giliran pemain
+    //private void UpdatePemainList()
+    //{
+    //    foreach (GameObject pemain in pemainList)
+    //    {
+    //        if (pemain != null)
+    //        {
+    //            Pemain pemainComponent = pemain.GetComponent<Pemain>();
+    //            if (pemainComponent != null)
+    //            {
+    //                // Nonaktifkan pemain lain
+    //                pemain.SetActive(false);
+    //            }
+    //        }
+    //    }
+    //}
 }
